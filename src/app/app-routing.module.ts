20afbbf2e8ede5776c3cf7ea42eb6bc01f84796f
CuddlyBunion341 <daniel@bengl.cz>
2023-07-01 00:00:34 +0200

    added routes for list and detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostListComponent } from './post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/:id', component: PostDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
